7cd343ca7fe77db82bed1863a27a983ff64142b2
Joey DeSantis <desantjj@mail.uc.edu>
2021-03-03 04:09:48 +0000

    formatting

--- Main.java
+++ Main.java
@@ -2,5 +2,15 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
     System.out.println("My name is Joey DeSantis");
+
+    int x = 5;
+    int y = 7;
+
+    if (x < y) {
+      System.out.println("Smaller");
+    }
+    else {
+      System.out.println("Bigger");
+    }
   }
 }
